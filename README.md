ed022e73f2235bb31dd1aaec07c8424eb7e77dcb
Adel Moreno <adel.argenix@gmail.com>
2018-07-14 18:18:38 -0600

    made the basic setup for pwp project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # adels-tech-profile
+A website to demonstrate my web development skills.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adels
+ * Date: 7/14/2018
+ * Time: 6:15 PM
+ */
