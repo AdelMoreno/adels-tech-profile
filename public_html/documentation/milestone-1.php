aeaaf956f32f6435ed5ff6120f959c17d9df4ae1
Adel Moreno <adel.argenix@gmail.com>
2018-07-15 17:08:37 -0600

    updated purpose, audience, and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adels
- * Date: 7/14/2018
- * Time: 6:15 PM
- */
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Adel's Web Development </title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, and Goal</h1>
+		<ul>
+			<li><b>Purpose&#58; To present my web development skills to potential employers, and in doing so gain full time employment.</b></li>
+			<li><b>Audience&#58;</b> Potential employers who are looking for a Jr. Web Developer.</li>
+			<li><b>Goal&#58;</b> To obtain full time employment as a Jr. Web Developer.</li>
+		</ul>
+	</body>
+</html>
