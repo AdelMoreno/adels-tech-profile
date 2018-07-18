5648544aeccad2a0a292c06a284711708af48e97
Adel Moreno <adel.argenix@gmail.com>
2018-07-17 21:44:32 -0600

    updated persona only missing scenario

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,9 +7,30 @@
 	<body>
 		<h1>Purpose, Audience, and Goal</h1>
 		<ul>
-			<li><b>Purpose&#58; To present my web development skills to potential employers, and in doing so gain full time employment.</b></li>
-			<li><b>Audience&#58;</b> Potential employers who are looking for a Jr. Web Developer.</li>
-			<li><b>Goal&#58;</b> To obtain full time employment as a Jr. Web Developer.</li>
+			<li>Purpose&#58; <p>To present my web development skills to potential employers, and in doing so gain full time employment.</p></li>
+			<li>Audience&#58; Potential employers who are looking for a Jr. Web Developer.</li>
+			<li>Goal&#58; To obtain full time employment as a Jr. Web Developer.</li>
+		</ul>
+		<h2>Persona</h2>
+		<ul>
+			<li>Name&#58; <p>Elizabeth Samuels</p></li>
+			<li>Gender&#58; <p>Female</p></li>
+			<li>Age&#58; <p>32</p></li>
+			<li>Occupation&#58; <p>H&R manager and part-time coder for LAVU.</p></li>
+			<li>Personality&#58; <p>She had friendly demeanor.
+					She doesn't let stress get to her.
+				She tends to be very positive.</p></li>
+			<li>Devices&#58; <p>She uses an android phone running Oreo.
+				She also has a Dell Inspiron 17 with Intel i7 processor.
+				She uses this mainly for work and the little coding projects she has to do.</p></li>
+			<li>Skill Level&#58; <p>She is pretty skilled with just about all electronics.
+				She doesn't have advanced skills when it comes to coding.
+				But, having basic knowledge allows her to scope out potential candidates.</p></li>
+			<li>Frustrations&#58; <p>Unable to find someone willing to learn more about coding.
+				Unable to find someone willing to relocate.
+				Unable to find someone who has an open mind when it comes to different coding styles.</p></li>
+			<li>Goal&#58; <p>To hire a new employee who had the drive to learn and help the company grow.</p></li>
+			<li>Scenario&#58; <p></p></li>
 		</ul>
 	</body>
 </html>
