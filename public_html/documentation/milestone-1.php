8b105ab96fc90a805400aca35e2ba6d176577ae5
Adel Moreno <adel.argenix@gmail.com>
2018-07-18 08:34:51 -0600

    updated to include scenario as well as some minor layout fixes

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,20 +7,23 @@
 	<body>
 		<h1>Purpose, Audience, and Goal</h1>
 		<ul>
-			<li>Purpose&#58; <p>To present my web development skills to potential employers, and in doing so gain full time employment.</p></li>
+			<li>Purpose&#58; To present my web development skills to potential employers, and in doing so gain full time employment.</li>
 			<li>Audience&#58; Potential employers who are looking for a Jr. Web Developer.</li>
 			<li>Goal&#58; To obtain full time employment as a Jr. Web Developer.</li>
 		</ul>
 		<h2>Persona</h2>
 		<ul>
-			<li>Name&#58; <p>Elizabeth Samuels</p></li>
-			<li>Gender&#58; <p>Female</p></li>
-			<li>Age&#58; <p>32</p></li>
-			<li>Occupation&#58; <p>H&R manager and part-time coder for LAVU.</p></li>
-			<li>Personality&#58; <p>She had friendly demeanor.
-					She doesn't let stress get to her.
-				She tends to be very positive.</p></li>
-			<li>Devices&#58; <p>She uses an android phone running Oreo.
+			<li>Name&#58; Elizabeth Samuels</li>
+			<li>Gender&#58; Female</li>
+			<li>Age&#58; 32</li>
+			<li>Occupation&#58; H&R manager and part-time coder for Rural Sourcing, INC.</li>
+			<li>Personality&#58;
+				<ul>
+					<li>She has a friendly demeanor.</li>
+					<li>She doesn't let stress get to her.</li>
+					<li>She tends to be very positive and is looking for someone who is the same.</li>
+				</ul>
+			<li>Devices&#58; <p>She uses Samsung s9+ running Android Oreo.
 				She also has a Dell Inspiron 17 with Intel i7 processor.
 				She uses this mainly for work and the little coding projects she has to do.</p></li>
 			<li>Skill Level&#58; <p>She is pretty skilled with just about all electronics.
@@ -29,8 +32,17 @@
 			<li>Frustrations&#58; <p>Unable to find someone willing to learn more about coding.
 				Unable to find someone willing to relocate.
 				Unable to find someone who has an open mind when it comes to different coding styles.</p></li>
-			<li>Goal&#58; <p>To hire a new employee who had the drive to learn and help the company grow.</p></li>
-			<li>Scenario&#58; <p></p></li>
+			<li>Goal&#58; <p>To hire a new employee who has the drive to learn and help the company grow.</p></li>
+			<li>Scenario&#58;
+				<ul>
+					<li>1. Elizabeth will navigate to my website via Linkedin.</li>
+					<li>2. She will see a link to my Github as well as a link to download a PDF for my resume.</li>
+					<li>3. She will click on the link to my resume and it will start the download automatically.</li>
+					<li>4. She will look over my Resume and be impressed that I graduated from Deep Dive Coding.</li>
+					<li>5. She will then explore my website further and will notice that I have a box containing preview pictures of projects I've worked on.</li>
+					<li>6. She will be very impressed and will use the phone number on my resume to schedule an interview!</li>
+				</ul>
+			</li>
 		</ul>
 	</body>
 </html>
