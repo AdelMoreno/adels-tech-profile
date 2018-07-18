00d53eae30bd2f49b252f530d8a87f25319b2412
Adel Moreno <adel.argenix@gmail.com>
2018-07-18 16:48:56 -0600

    added milestone-2.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,48 +6,48 @@
 		<title>Adel's Web Development </title>
 	</head>
 	<body>
-		<img src="elizabeth%20samuels.jpg" class="elizabeth" alt="Elizabeth">
+		<img src="../../elizabeth%20samuels.jpg" class="elizabeth" alt="Elizabeth">
 		<div class="text">
-		<h1>Purpose, Audience, and Goal</h1>
-		<ul>
-			<li>Purpose&#58; To present my web development skills to potential employers, and in doing so gain full time employment.</li>
-			<li>Audience&#58; Potential employers who are looking for a Jr. Web Developer.</li>
-			<li>Goal&#58; To obtain full time employment as a Jr. Web Developer.</li>
-		</ul>
-		<h2>Persona</h2>
-		<ul>
-			<li>Name&#58; Elizabeth Samuels</li>
-			<li>Gender&#58; Female</li>
-			<li>Age&#58; 32</li>
-			<li>Occupation&#58; H&R manager and part-time coder for Rural Sourcing, INC.</li>
-			<li>Personality&#58;
-				<ul>
-					<li>She has a friendly demeanor.</li>
-					<li>She doesn't let stress get to her.</li>
-					<li>She tends to be very positive and is looking for someone who is the same.</li>
-				</ul>
-			<li>Devices&#58; <p>She uses Samsung s9+ running Android Oreo.
-				She also has a Dell Inspiron 17 with Intel i7 processor.
-				She uses this mainly for work and the little coding projects she has to do.</p></li>
-			<li>Skill Level&#58; <p>She is pretty skilled with just about all electronics.
-				She doesn't have advanced skills when it comes to coding.
-				But, having basic knowledge allows her to scope out potential candidates.</p></li>
-			<li>Frustrations&#58; <p>Unable to find someone willing to learn more about coding.
-				Unable to find someone willing to relocate.
-				Unable to find someone who has an open mind when it comes to different coding styles.</p></li>
-			<li>Goal&#58; <p>To hire a new employee who has the drive to learn and help the company grow.</p></li>
-			<li>User Story&#58; <p>Elizabeth is looking for a candidate that has some training and a website to show off what they know.</p></li>
-			<li>Scenario&#58;
-				<ol>
-					<li> Elizabeth will navigate to my website via Linkedin.</li>
-					<li> She will see a link to my Github as well as a link to download a PDF for my resume.</li>
-					<li> She will click on the link to my resume and it will start the download automatically.</li>
-					<li> She will look over my Resume and be impressed that I graduated from Deep Dive Coding.</li>
-					<li> She will then explore my website further and will notice that I have a box containing preview pictures of projects I've worked on.</li>
-					<li> She will be very impressed and will use the phone number on my resume to schedule an interview!</li>
-				</ol>
-			</li>
-		</ul>
+			<h1>Purpose, Audience, and Goal</h1>
+			<ul>
+				<li>Purpose&#58; To present my web development skills to potential employers, and in doing so gain full time employment.</li>
+				<li>Audience&#58; Potential employers who are looking for a Jr. Web Developer.</li>
+				<li>Goal&#58; To obtain full time employment as a Jr. Web Developer.</li>
+			</ul>
+			<h2>Persona</h2>
+			<ul>
+				<li>Name&#58; Elizabeth Samuels</li>
+				<li>Gender&#58; Female</li>
+				<li>Age&#58; 32</li>
+				<li>Occupation&#58; H&R manager and part-time coder for Rural Sourcing, INC.</li>
+				<li>Personality&#58;
+					<ul>
+						<li>She has a friendly demeanor.</li>
+						<li>She doesn't let stress get to her.</li>
+						<li>She tends to be very positive and is looking for someone who is the same.</li>
+					</ul>
+				<li>Devices&#58; <p>She uses Samsung s9+ running Android Oreo.
+					She also has a Dell Inspiron 17 with Intel i7 processor.
+					She uses this mainly for work and the little coding projects she has to do.</p></li>
+				<li>Skill Level&#58; <p>She is pretty skilled with just about all electronics.
+					She doesn't have advanced skills when it comes to coding.
+					But, having basic knowledge allows her to scope out potential candidates.</p></li>
+				<li>Frustrations&#58; <p>Unable to find someone willing to learn more about coding.
+					Unable to find someone willing to relocate.
+					Unable to find someone who has an open mind when it comes to different coding styles.</p></li>
+				<li>Goal&#58; <p>To hire a new employee who has the drive to learn and help the company grow.</p></li>
+				<li>User Story&#58; <p>Elizabeth is looking for a candidate that has some training and a website to show off what they know.</p></li>
+				<li>Scenario&#58;
+					<ol>
+						<li> Elizabeth will navigate to my website via Linkedin.</li>
+						<li> She will see a link to my Github as well as a link to download a PDF for my resume.</li>
+						<li> She will click on the link to my resume and it will start the download automatically.</li>
+						<li> She will look over my Resume and be impressed that I graduated from Deep Dive Coding.</li>
+						<li> She will then explore my website further and will notice that I have a box containing preview pictures of projects I've worked on.</li>
+						<li> She will be very impressed and will use the phone number on my resume to schedule an interview!</li>
+					</ol>
+				</li>
+			</ul>
 		</div>
 	</body>
 </html>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+
+	</body>
+	</html>
