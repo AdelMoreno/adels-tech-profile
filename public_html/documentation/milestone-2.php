fab5e8805a6f6e3cbb2b544ed02827bb89c2ea85
Adel Moreno <adel.argenix@gmail.com>
2018-07-31 13:10:22 -0600

    added content-strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,6 +5,17 @@
 		<title>Milestone 2</title>
 	</head>
 	<body>
+		<h1>Content-Strategy</h1>
+			<p>My site will be a one page website and have the following sections.<br>
+				<b>Header&#58;</b> This will have picture of me. It will also have my name and my job title as a web developer.
+				Near this section I will have links that jump to each section that will allow for quick navigation.<br>
+				<b>About Me&#58;</b> This will have information on me, such as my technical background and the fact that I attended Deep Dive Coding.
+				I will also list the languages that I have a basic understanding of. I will also include some of interests to help let my personality shine through.<br>
+				<b>Portfolio&#58;</b> This section will have an interactive image gallery of my best projects. I would like to incorporate
+				photo bucket somehow.<br>
+				<b>Contact</b> This section will have icons and links to my GitHub, Twitter, and LinkedIn. I will also have a contact form
+				that will allow someone to fill out some information and send me a message via e-mail.
+			</p>
 
 	</body>
 	</html>
