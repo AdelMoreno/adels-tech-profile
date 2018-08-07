433e140d957f45fb9a36387bd90650cf213a7798
Adel Moreno <adel.argenix@gmail.com>
2018-08-06 19:05:53 -0600

    fixed resume so it opens in a new tab and moved up with the rest of my links

---
+++ public_html/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link href="styles.css" rel="stylesheet" type="text/css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha256-98vAGjEDGN79TjHkYWVD4s87rvWkdWLHPs5MC3FvFX4=" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<title>Adel's Tech Profile</title>
+	</head>
+	<body>
+
+		<header class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<img class="img-fluid p-3" src="img_adel.jpg">
+				</div>
+				<div class="col-md-7 align-self-center align-right">
+					<p>Adel Moreno</p>
+				</div>
+				<div class="col-md-2 align-self-center">
+					<p>Web Developer</p>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md align-self-center">
+			<h2>About Me</h2>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md align-self-center">
+			<p>This area will be all about me!</p>
+		</div>
+	</div>
+		</div>
+	</body>
