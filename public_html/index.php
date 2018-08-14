ec7d01b98b9e8812e75147be8753ba68e21c330c
Adel Moreno <adel.argenix@gmail.com>
2018-08-13 18:59:53 -0600

    added link to github and fontawesome CDN

--- public_html/index.php
+++ public_html/index.php
@@ -5,15 +5,17 @@
 		<meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no">
 
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 		<link href="styles.css" rel="stylesheet" type="text/css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha256-98vAGjEDGN79TjHkYWVD4s87rvWkdWLHPs5MC3FvFX4=" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 		<title>Adel's Tech Profile</title>
 	</head>
 	<body>
 
-		<header class="container-fluid" id="home">
+		<header class="container-fluid mb-5" id="home">
 			<div class="row">
 				<div class="col-md-2">
 					<img class="img-fluid p-3" src="img_adel.jpg">
@@ -21,7 +23,7 @@
 				<div class="header-text col-md-3 align-self-center ml-auto">
 					<p>Adel Moreno</p>
 				</div>
-				<div class="header-text col-md-2 align-self-center mr-auto">
+				<div class="header-text col-md-3 align-self-center mr-auto">
 					<p>Web Developer</p>
 				</div>
 			</div>
@@ -43,7 +45,10 @@
 							<a class="nav-link" href="#portfolio">Portfolio |</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#contact">Contact</a>
+							<a class="nav-link" href="#contact">Contact |</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="adel_moreno_resume.pdf" target="_blank">Resume</a>
 						</li>
 					</ul>
 				</div>
@@ -56,7 +61,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid text-center">
+		<div class="container-fluid text-center mb-5">
 			<div class="row">
 				<div class="col-md">
 			<p>This area will be all about me!</p>
@@ -70,7 +75,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid text-center">
+		<div class="container-fluid text-center mb-5">
 		<div id="carousel" class="carousel slide" data-ride="carousel">
 			<div class="carousel-inner">
 				<div class="carousel-item active">
@@ -95,8 +100,11 @@
 		</div>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-4 text-center">
+				<div class="col-md-4 text-center align-middle">
 					<h2 class="section-text" id="contact">Contact</h2>
+					<i class="fab fa-github-alt fa-spin fa-2x"></i>
+					<a href="https://github.com/AdelMoreno">github</a><br>
+
 				</div>
 			<form class="col-md-6 ml-auto mt-2">
 				<div class="form-group">
@@ -111,7 +119,7 @@
 				</div>
 				<div class="form-group">
 					<div class="col-md-8">
-						<input type="text" class="form-control" placeholder="Subject" required>
+						<input type="text" class="form-control" placeholder=" Subject" required>
 					</div>
 				</div>
 				<div class="form-group">
