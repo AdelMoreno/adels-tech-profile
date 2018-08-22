1a2db3e2f3fa4a1034054f4dc8a423fbedc06430
Adel Moreno <adel.argenix@gmail.com>
2018-08-22 16:41:23 -0600

    added label placeholders and slide indicators

--- public_html/index.php
+++ public_html/index.php
@@ -70,15 +70,29 @@
 		</div>
 		<div class="container-fluid smaller text-center portfolio mb-5">
 		<div id="carousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselIndicators" data-slide-to="2"></li>
+			</ol>
 			<div class="carousel-inner">
 				<div class="carousel-item active">
 					<img class="d-block w-100" src="http://placekitten.com/300/200" alt="First slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>First Label</h3>
+					</div>
 				</div>
 				<div class="carousel-item">
 					<img class="d-block w-100" src="http://placekitten.com/250/200" alt="Second slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Second Label</h3>
+					</div>
 				</div>
 				<div class="carousel-item">
 					<img class="d-block w-100" src="http://placekitten.com/280/200" alt="Third slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Third Label</h3>
+					</div>
 				</div>
 			</div>
 			<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
